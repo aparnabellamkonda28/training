38c1cd3dfdc59271c59656313e4b21256cf2cf99
aparnabellamkonda28 <aparnabellamkonda24@gmail.com>
2025-01-23 15:42:04 +0530

    Update async.js

--- async.js
+++ async.js
@@ -43,3 +43,26 @@ function fetchUsers()
     .catch(err=>console.log(err))
 }
 fetchUsers();
+
+let p = new Promise(resolve,(reject)=>{
+    setTimeout(()=>{
+        resolve("Promise successfully completing");
+    } , 3000)
+})
+
+function demo()
+{
+    console.log("Stsrt");
+    let x = p;
+    console.log(x);
+
+    console.log("End")
+}
+demo();
+
+async function fetchUsers() {
+    let response=await fetch ("https://jsonplaceholder.typicode.com/users");
+    let data=await response.json();
+    console.log(data);   
+}
+fetchUsers();
